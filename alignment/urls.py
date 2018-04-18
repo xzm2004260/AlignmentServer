2d6aabb5dd83a690c4bbc18276588b178dc3afbe
mirzabaig <musharaf715@gmail.com>
2018-04-18 21:37:40 +0500

    added upload view

--- alignment/urls.py
+++ alignment/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import CreateAlignmentAPIView, AlignmentDetailAPIView
+from .views import (
+    CreateAlignmentAPIView,
+    AlignmentDetailAPIView,
+    UploadAPIView
+)
 
 
 urlpatterns = [
     url('^$', CreateAlignmentAPIView.as_view(), name='create-alignment'),
+    url('^upload$', UploadAPIView.as_view(), name='upload-audio'),
     url('^(?P<pk>[0-9]+)$', AlignmentDetailAPIView.as_view(), name='alignment-detail'),
 ]
 
--- services/utils.py
+++ services/utils.py
@@ -1,5 +1,29 @@
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
+import os
+import subprocess
 
 
 def update_filename(instance, filename):
     return 'lyrics/{}'.format(instance.id)
 
+
+def get_file(recording_URL, alignment_id,  outputDir):
+    if not os.path.isdir(outputDir):
+        os.mkdir(outputDir)
+    # TODO: get automatically file type
+    source_file_URI = os.path.join(outputDir, alignment_id + '.mp3')
+    response = urlopen(recording_URL)
+    a = response.read()
+    with open(source_file_URI, 'w') as f:
+        f.write(a)
+
+    wav_file_URI = os.path.join(outputDir, alignment_id + '.wav')
+    if os.path.isfile(wav_file_URI):
+        return
+
+    pipe = subprocess.Popen(['/usr/local/bin/ffmpeg', '-i', source_file_URI, wav_file_URI])
+    pipe.wait()
